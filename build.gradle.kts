54810b217e0827639b82c902fde23d47120e2f83
M.Higashijima <boctok.ctpoba@gmail.com>
2020-11-18 15:30:40 +0900

    generateProtoで生成できるもの

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+import com.google.protobuf.gradle.*
+import org.gradle.kotlin.dsl.provider.gradleKotlinDslOf
+
 plugins {
 	id("org.springframework.boot") version "2.3.5.RELEASE"
 	id("io.spring.dependency-management") version "1.0.10.RELEASE"
 	kotlin("jvm") version "1.3.72"
 	kotlin("plugin.spring") version "1.3.72"
+	id("com.google.protobuf") version "0.8.8"
 }
 
 group = "jp.boctok"
@@ -15,7 +19,18 @@ repositories {
 	mavenCentral()
 }
 
+sourceSets{
+	create("kotlingrpc"){
+		proto{
+			srcDir("src/main/kotlin/protobuf")
+		}
+	}
+}
+
 dependencies {
+	implementation("com.google.protobuf:protobuf-java:3.6.1")
+	implementation("io.grpc:grpc-stub:1.15.1")
+	implementation("io.grpc:grpc-protobuf:1.15.1")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
@@ -23,6 +38,26 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
+}
+
+protobuf{
+	protoc{
+		artifact = "com.google.protobuf:protoc:3.6.1"
+	}
+	plugins{
+		id("grpc"){
+			artifact = "io.grpc:protoc-gen-grpc-java:1.15.1"
+		}
+	}
+	generateProtoTasks{
+		ofSourceSet("main").forEach{
+			it.plugins{
+				id("grpc")
+			}
+		}
+	}
+	// 生成するソースをsrc側に作る
+	generatedFilesBaseDir = "$projectDir/src"
 }
 
 tasks.withType<Test> {
